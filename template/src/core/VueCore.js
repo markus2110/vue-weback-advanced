308267766f7450798c6e534d89437077e5a87dee
Markus Sommerfeld <msommerfeld@silversurfer7.de>
2018-01-05 17:34:56 +0100

    router / store plugin moved to VueCore

--- template/src/core/VueCore.js
+++ template/src/core/VueCore.js
@@ -1,9 +1,18 @@
 import Vue          from "vue";
 import VueExtender  from "./plugins/VueExtender";
+import Router       from "@/router/index.js";
+import Store        from "@/store/index.js";
+
 
 Vue.use(VueExtender);
 
+
 export default Vue.extend({
+    // Vue-Router
+    router :        Router,
+
+    // VueX
+    store :         Store,
     
     beforeCreate : function(){
         this.$initPackages();
--- template/src/main.js
+++ template/src/main.js
@@ -1,6 +1,5 @@
 import VueApp       from "./core/VueCore.js";
-import Router       from "./router/index.js";
-import Store        from "./store/index.js";
+
 import AppMain      from "./views/app.vue";
 
 
@@ -9,12 +8,8 @@ import DemoPackage from "packages/Demo/index";
 
 
 var App = new VueApp({
-    router :        Router,
-    store :         Store,
-
     // The active Vue Packages
     activePackages : [ DemoPackage ],
-    
     // h = alias for createElement
     render :        (h) => h(AppMain)
 })
