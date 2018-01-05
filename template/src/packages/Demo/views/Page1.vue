46509e57008280be45cd17e6bed0dac13432648f
Markus Sommerfeld <msommerfeld@silversurfer7.de>
2018-01-05 17:32:44 +0100

    changed to display the lorem ipsum data

--- template/src/packages/Demo/views/Page1.vue
+++ template/src/packages/Demo/views/Page1.vue
@@ -1,10 +1,30 @@
 <template>
     <div>
         <h2>The Demo Component Pag1</h2>
+
+
+        <div v-if="demoRecords.length > 0">
+            <p v-for="record in demoRecords">\{{ record }}</p>
+        </div>
+        <div  v-else>
+            <span>Loading ...</span>
+        </div>
+
     </div>
 </template>
 
 <script>
     export default {
+
+        created: function () {
+            this.$store.dispatch('Lorem/fetchRecords');
+        },
+
+        computed: {
+            demoRecords : function(){
+                return this.$store.getters["Lorem/allRecords"];
+            }
+        }
+
     }
 </script>
