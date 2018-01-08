a0817ed0f59c91340974f942c5b231487f3e9afc
Markus Sommerfeld <msommerfeld@silversurfer7.de>
2018-01-08 18:26:14 +0100

    some comments added

--- template/src/main.js
+++ template/src/main.js
@@ -1,11 +1,12 @@
+// The CoreVue Class
 import VueApp       from "./core/VueCore.js";
 
-import AppMain      from "./views/app.vue";
-
-
+// Packages
 import DemoPackage from "packages/Demo/index";
 import UserPackage from "packages/User/index";
 
+// The Main App Template
+import AppMain      from "./views/app.vue";
 
 
 var App = new VueApp({
