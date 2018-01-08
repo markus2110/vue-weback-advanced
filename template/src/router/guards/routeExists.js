8bae253645180002f1878086a35d2fd81d529040
Markus Sommerfeld <msommerfeld@silversurfer7.de>
2018-01-08 15:34:05 +0100

    changed to register all routes / stores before the Vue instance is created

--- template/src/router/guards/routeExists.js
+++ template/src/router/guards/routeExists.js
@@ -1,4 +1,5 @@
 export default function RouteExistsGuard(to, from, next){
+        console.info("RouteExists guard triggered");
     if(to.matched.some( (record) => true )){
         next();
     }else{
